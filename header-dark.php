dfd76921e0f83a83eab8bb5c6212a515f5d9ff8f
Feroz <mdferozhossain39@gmail.com>
2025-06-14 23:22:44 +0600

    Update meta

--- header-dark.php
+++ header-dark.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
 <?php wp_head(); ?>

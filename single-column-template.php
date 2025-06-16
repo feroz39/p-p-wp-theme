debff0502aa008997aa8013321a40be4dd58e7a6
Feroz <mdferozhossain39@gmail.com>
2025-06-16 15:50:33 +0600

    Fixed gravity form

--- single-column-template.php
+++ single-column-template.php
@@ -14,7 +14,11 @@
         
         <section class="px-8 pt-24">
             <div class="max-w-screen-md mx-auto">
-                <?php get_template_part( 'parts/post-body', null, ['content'=>get_the_content()]); ?>
+                <div
+                    class="prose prose-lg text-brand prose-p:font-light prose-img:rounded-md prose-p:text-brand prose-a:no-underline hover:prose-a:underline prose-a:text-orange prose-strong:text-brand prose-h2:font-serif prose-h2:text-brand prose-h3:text-xl prose-h3:font-medium prose-h2:font-medium prose-h3:text-orange prose-h2:mt-6 prose-h3:font-sans prose-strong:font-medium prose-ul:font-sans prose-ul:font-light prose-p:font-sans prose-h4:text-brand"
+                >
+                    <?php the_content( ); ?>
+                </div>
             </div>
         </section>
         

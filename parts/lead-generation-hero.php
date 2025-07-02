6cd65a2d885f859161750656d73573a65724f2ee
Feroz <mdferozhossain39@gmail.com>
2025-07-02 12:28:25 +0600

    change home hero placeholder and adjust form width and padding on lead generation hero

--- parts/lead-generation-hero.php
+++ parts/lead-generation-hero.php
@@ -1,4 +1,5 @@
 <?php if (get_row_layout() === 'lead_generation_hero'): ?>
+<!-- ACF: Lead Generation Hero -->
 <?php 
 $title = get_sub_field('title');
 $subtitle = get_sub_field('subtitle');
@@ -8,7 +9,7 @@ $cover_image = get_sub_field('cover_image');
 ?>
 
 <div>
-	<section class="relative z-10 md:min-h-screen h-auto md:max-h-[1100px] md:h-[900px] md:px-12 px-6 flex flex-col-reverse md:flex-col bg-brand/80 py-36 md:py-0">
+	<section class="relative z-10 md:min-h-screen h-auto md:max-h-[1100px] md:h-[1000px] md:px-12 px-6 flex flex-col-reverse md:flex-col bg-brand/80 py-36 md:py-0">
 		<div class="relative w-full h-full max-w-screen-xl mx-auto">
 			<div class="flex items-center md:w-7/12 lg:w-6/12 xl:w-6/12 2xl:w-5/12 md:absolute md:inset-0">
 				<div>
@@ -28,8 +29,8 @@ $cover_image = get_sub_field('cover_image');
 					</h2><?php endif; ?>
 				</div>
 			</div>
-			<div class="flex items-center h-full mt-10 md:mt-0">
-				<div class="right-0 w-full text-white rounded-lg md:absolute md:w-4/12 bg-brand">
+			<div class="flex items-center h-full mt-10 md:pt-28 md:mt-0">
+				<div class="right-0 w-full text-white rounded-lg md:absolute md:w-5/12 bg-brand">
 					<div class="relative z-10 max-w-screen-xl p-10 mx-auto text-sm">
 						<div class="font-sans prose-sm prose text-white gfield-HTML prose-p:font-light prose-img:rounded-md prose-p:text-white prose-a:no-underline hover:prose-a:underline prose-a:text-orange prose-strong:text-brand prose-h2:font-serif prose-h2:text-orange prose-h3:text-lg prose-h3:font-medium prose-h2:font-medium prose-h2:text-4xl prose-h2:leading-10">
 							<div class="dark-gf">
@@ -50,7 +51,7 @@ $cover_image = get_sub_field('cover_image');
 		height="600" 
 		decoding="async" 
 		data-nimg="1" 
-		class="absolute inset-0 -z-10 h-auto min-h-screen md:max-h-[1100px] md:h-[900px] object-cover grayscale w-full" 
+		class="absolute inset-0 -z-10 h-auto min-h-screen md:max-h-[1100px] md:h-[1000px] object-cover grayscale w-full" 
 		src="<?= esc_attr( $cover_image['url'] ); ?>"
 	><?php endif; ?>
 </div>
